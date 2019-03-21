00bc609956fb5140d308259e758b9548aa99b4b3
Mhrpatel85 <29385964+Mhr85@users.noreply.github.com>
2019-03-20 23:08:57 -0400

    created divs in jQuery, created all 3 slides requred, added two question, added radio buttons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# triviaGame
+ # triviaGame
 
 html
 create container-fluid in body
@@ -20,3 +20,14 @@ create an on click event for the start button to run the startGame button
 
 <!-- QUESTIONS FOR TA -->
 <!-- 1. What code do I put inside the document.ready function -->
+
+
+create one object with Q1. with question = ["question?"], answer = ["0", "1", "2", "3"], result = [first array is an correct answer], call the object and save the correct answer in a var temp
+
+use javascript array shuffle random for the answers
+
+
+create div for each questions
+add h3 for each questions
+label for radio buttons
+append radio buttons to that div
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,15 +6,37 @@ var intervalId
 var clockRunning = false;
 var time = 0;
 // STARTING DIV
-window.onload = function(){
+// window.onload = function(){
+var divOne = $("<div></div>");
+divOne.addClass("container bg-warning container-style");
+divOne.attr('id', 'starting-div');
+$("#main-container").append(divOne);
+
+var divTwo = $("<div></div>");
+divTwo.addClass("container bg-warning container-style");
+divTwo.attr('id', 'game-div');
+$("#main-container").append(divTwo);
+$("#game-div").html("<h1>Trivia Game</h1> <h2>00:00</h2> <p class='q-1'></p> <br> <p class='q-2'></p>");
+$("#game-div h2").attr('id', 'display');
+
+
+
+var divThree = $("<div></div>");
+divThree.addClass("container bg-warning container-style");
+divThree.attr('id', 'result-div');
+$("#main-container").append(divThree);
+$("#result-div").html("<h1>Trivia Game</h1>");
+
 var startButton = $("<button></button>");
 startButton.text("Start");
 startButton.addClass("btn btn-dark btn-block p-4");
-$("#starting-div").html("<h1>Trivia Game</h1>")
+$("#starting-div").html("<h1>Trivia Game</h1>");
 $("#starting-div").append(startButton);
 $(".btn").on("click", startTimer);
+// $("#starting-div").hide();
+// startTimer();
+// };
 
-};
 
 // function startTheGame(){
   function startTimer(){
@@ -28,6 +50,7 @@ $(".btn").on("click", startTimer);
     time++;
     var newTime = timeConverter(time);
     // $("#starting-div h1").text(newTime);
+    // $("#game-div").text("<h1>Trivia Game</h1>");
     $("#display").text(newTime);
   }
 
@@ -47,3 +70,36 @@ $(".btn").on("click", startTimer);
     return minutes + ":" + seconds;
   }
 // }
+
+var triviaQuestions = {
+  questionOne: "You should eat fruits and vegetables because",
+  optionsOne: [
+    "They contain fiber, which helps keep your digestive system healthy",
+    "They give you energy",
+    "They contain vitamins and minerals that help you stay healthy",
+    "All of the above",
+  ],
+  correctAnswer: "All of the above",
+
+  questionTwo: "More than 250 different kinds of fruit and vegetables are grown in California. However, a few fruits and vegetables require a more tropical environment to grow. Which fruit is not grown very often in California?",
+  optionsTwo: [
+    "banana",
+    "plum",
+    "grape",
+    "strawberry",
+  ],
+
+}
+
+$(".q-1").append(triviaQuestions.questionOne);
+for(var i = 0; i < triviaQuestions.optionsOne.length; i++){
+  $('<input type="radio" name="radioButton" class="radio-1"/>').appendTo("#game-div");
+  $("input:radio").text(triviaQuestions.optionsOne[0]);
+}
+
+
+$(".q-2").append(triviaQuestions.questionTwo);
+for(var i = 0; i < triviaQuestions.optionsTwo.length; i++){
+  $('<input type="radio" name="radioButton" class="radio-1"/>').appendTo("#game-div");
+  $("input:radio").text(triviaQuestions.optionsOne[0]);
+}
